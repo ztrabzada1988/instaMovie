e9c84d9519016ce1b5b91ce6103322bfe7d6519b
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-04-21 16:07:18 -0700

    updating react components and api

--- js/components/App.js
+++ js/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import Request from 'superagent';
+import Request from 'superagent';
 
 
 class App extends React.Component {
@@ -10,6 +10,13 @@ class App extends React.Component {
 
   componentWillMount() {
     // Called the first time the component is loaded right before the component is added to the page
+    var url = "https://api.themoviedb.org/3/search/movie?api_key=508b14c0d7be622882e5f6d6221dd78f&language=en-US&query=star&page=10&include_adult=false";
+    Request.get(url).then((response) =>{
+      this.setState({
+        movies: response.body.results,
+        total: response.total_results
+      })
+    })
   }
 
   componentDidMount() {
@@ -20,20 +27,12 @@ class App extends React.Component {
     // called when the component is removed.
   }
 
-  search(e) {
-    console.log("onChange search function is connected");
-  }
-
-  submitSearch(e) {
-    console.log("button is connected");
-  }
-
   render() {
     return (
       <div className="container">
         <form className="form-inline">
           <input type="text" onChange={(e) => this.search()} className="form-control" placeholder="Type a movie to search..." />
-          <input type="submit" onSubmit={(e) => this.submitSearch()} className="btn btn-success" value="Search" />
+          <input type="submit" className="btn btn-success" value="Search" />
         </form>
       </div>
     )
