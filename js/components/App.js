708b55a2e29f8f1497cc87880708d9e151303032
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-04-21 10:03:30 -0700

    adding API request functionality

--- js/components/App.js
+++ js/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Request from 'superagent';
 
 
 class App extends React.Component {
@@ -20,15 +20,24 @@ class App extends React.Component {
     // called when the component is removed.
   }
 
+  search(e) {
+      e.preventDefault();
+      Request.get(response, reject) {
+        
+      }
+  }
+
   render() {
     return (
       <div className="container">
         <form className="form-inline">
-          <input type="text" className="form-control" placeholder="Type a movie to search..." />
-          <input type="submit" className="form-control" value="Search" />
+          <input type="text" onChange={(e) => this.search()} className="form-control" placeholder="Type a movie to search..." />
+          <input type="submit" className="btn btn-success" value="Search" />
         </form>
       </div>
     )
   }
 
 }
+
+export default App
