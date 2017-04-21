cd5d08d1f6ecae50dd35bf5968a0addb1e9c7707
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-04-20 18:17:19 -0700

    updating components

---
+++ js/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+    // Called the first time the component is loaded right before the component is added to the page
+  }
+
+  componentDidMount() {
+    // called after the component has been rendered into the page
+  }
+
+  componentWillUnmount() {
+    // called when the component is removed.
+  }
+
+
+}
