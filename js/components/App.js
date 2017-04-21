15e4e6f93c8899327291e1ed9b1f3ae59f8e1168
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-04-21 16:34:57 -0700

    updating react components and api

--- js/components/App.js
+++ js/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Request from 'superagent';
-
+import _ from 'lodash';
 
 class App extends React.Component {
   constructor(){
@@ -10,13 +10,7 @@ class App extends React.Component {
 
   componentWillMount() {
     // Called the first time the component is loaded right before the component is added to the page
-    var url = "https://api.themoviedb.org/3/search/movie?api_key=508b14c0d7be622882e5f6d6221dd78f&language=en-US&query=star&page=10&include_adult=false";
-    Request.get(url).then((response) =>{
-      this.setState({
-        movies: response.body.results,
-        total: response.total_results
-      })
-    })
+    this.search();
   }
 
   componentDidMount() {
@@ -27,13 +21,39 @@ class App extends React.Component {
     // called when the component is removed.
   }
 
+  updateSearch() {
+    this.search(this.refs.query.value);
+  }
+
+  // search method will take as a default for query "star"
+  search(query = "star") {
+    var url = `https://api.themoviedb.org/3/search/movie?api_key=508b14c0d7be622882e5f6d6221dd78f&language=en-US&query=${query}&page=10&include_adult=false`;
+    Request.get(url).then((response) => {
+      this.setState({
+        movies: response.body.results,
+        total: response.total_results,
+      })
+    })
+  }
+
   render() {
+    // iterate over movies api and return poster of each movie
+    var movies = _.map(this.state.movies, (movie) => {
+      // put a key as the id of api result to avoid react child warning
+      return <li key={movie.id}>{movie.title}</li>
+    });
+
+    // just to see my api response
+    console.log(this.state.movies);
+
     return (
       <div className="container">
         <form className="form-inline">
-          <input type="text" onChange={(e) => this.search()} className="form-control" placeholder="Type a movie to search..." />
+          <input type="text" ref="query" onChange={(e) => this.updateSearch()} className="form-control" placeholder="Type a movie to search..." />
           <input type="submit" className="btn btn-success" value="Search" />
         </form>
+        <br/>
+        <ul>{movies}</ul>
       </div>
     )
   }
