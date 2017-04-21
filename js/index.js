ac6622ec939e8fbdaeb0900143abb74838f67bcd
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-04-20 18:05:39 -0700

    updating components

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
-require('babel-polyfill');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
 
+ReactDOM.render(
+      <App />,
+    document.getElementById('root')
+)
