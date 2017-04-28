e6328b64da113bbe896d67cedc0b3002393797ee
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-04-27 21:50:50 -0700

    updating react components and api

--- js/components/App.js
+++ js/components/App.js
@@ -25,6 +25,20 @@ class App extends React.Component {
       this.setState({
         movies: response.body.results,
         total: response.total_results,
+        movieID: data.id,
+        original_title: data.original_title,
+        tagline: data.tagline,
+        overview: data.overview,
+        homepage: data.homepage,
+        poster: data.poster_path,
+        production: data.production_companies,
+        production_countries: data.production_countries,
+        genre: data.genres,
+        release: data.release_date,
+        vote: data.vote_average,
+        runtime: data.runtime,
+        revenue: data.revenue,
+        backdrop: data.backdrop_path
       })
     })
   }
