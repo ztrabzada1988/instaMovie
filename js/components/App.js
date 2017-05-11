97ad609372fa403269102e0d472f54340d14234b
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-05-11 10:38:30 -0700

    updating App.js

--- js/components/App.js
+++ js/components/App.js
@@ -1,57 +1,28 @@
 import React from 'react';
-import Request from 'superagent';
-import _ from 'lodash';
+import SearchBar from './SearchBar';
+import { browserHistory } from 'react-router';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {};
-  }
 
-  componentWillMount() {
-    // Called the first time the component is loaded right before the component is added to the page
-    this.search();
-  }
-
-
-  updateSearch() {
-    this.search(this.refs.query.value)
-  }
-
-  search method will take as a default for query "star"
-  search(query = "star") {
-    var url = `https://api.themoviedb.org/3/search/movie?api_key=508b14c0d7be622882e5f6d6221dd78f&language=en-US&query=${query}&page=10&include_adult=false`;
-    Request.get(url).then((response) => {
-      this.setState({
-        movies: response.body.results,
-        total: response.total_results,
-      })
-    })
+  constructor(props) {
+    super(props);
   }
 
   render() {
-    // iterate over movies api and return poster of each movie
-    var movies = _.map(this.state.movies, (movie) => {
-      const imageUrl = `http://image.tmdb.org/t/p/w500/${movie.poster_path}`;
-      // put a key as the id of api result to avoid react child warning
-      return <li key={movie.id} className="list-items">{movie.title}<img src={imageUrl}/></li>
-    });
-
-    //just to see my api response
-    console.log(this.state.movies);
-
+    const title = 'InstaMovie';
     return (
-      <div className="container">
-        <form className="form-inline">
-          <input type="text" ref="query" onChange={(e) => this.updateSearch()} className="form-control" placeholder="Type a movie to search..." />
-          <input type="submit" className="btn btn-success" value="Search" />
-        </form>
-        <br/>
-        <ul>{movies}</ul>
+      <div>
+        <h1 className="webTitle">
+          <a href="/">{title}</a>
+        </h1>
+        <SearchBar />
+        <div>
+          {this.props.children}
+        </div>
       </div>
-    )
-  }
+    );
+  };
+};
 
-}
 
 export default App
