f42e7d4a94ed6302127b57decee9142dfc7e9f17
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-05-13 21:21:46 -0700

    adding reducers

---
+++ js/reducers/index.js
@@ -0,0 +1,35 @@
+import { SEARCH_MOVIE, RECEIVED_MOVIE, SHOW_DETAIL } from '../actions';
+
+const initialState = {
+  loading: false,
+  movies: []
+}
+
+export default (state=initialState, action) => {
+  switch(action.type) {
+    case SEARCH_MOVIE:
+
+    return {
+      ...state,
+      loading: true
+    }
+
+    case RECEIVED_MOVIE:
+
+    return {
+      movies: action.movies,
+      loading: false
+    }
+
+    case SHOW_DETAIL:
+
+      return {
+        ...state,
+        movie: state.movies.filter(i => i.id.toString() === action.movieId)[0]
+      }
+
+    default:
+      return state;
+  }
+
+}
