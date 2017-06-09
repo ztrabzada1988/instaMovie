018dc3f8571805dbbbfff66038cdc8ec21da865e
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-06-09 15:31:28 -0700

    updating files and debugging

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,13 @@ module.exports = {
     },
     devtool: 'inline-source-map',
     module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          loader: 'babel-loader'
-        },
-        
+      // loaders: [
+      //   {
+      //     test: /\.js$/,
+      //     exclude: /(node_modules)/,
+      //     loader: 'babel-loader'
+      //   },
+
       ]
     }
 };
