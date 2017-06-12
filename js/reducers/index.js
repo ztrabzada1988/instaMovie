904fca63ab37d4c96924613adc6c97e4d845f9ab
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-06-11 23:57:48 -0700

    updating files and debugging

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -25,7 +25,7 @@ export default (state=initialState, action) => {
 
       return {
         ...state,
-        movie: state.movies.filter(i => i.id.toString() === action.movieId)[0]
+        movie: state.movies.filter(i => i.id.toString() === action.movieId)//[0]
       }
 
     default:
